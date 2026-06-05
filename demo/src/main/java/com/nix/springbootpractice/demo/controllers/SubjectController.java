31c1088d7bc4b2cc8a1b0b39467054d9b8afd3af
nix <decastrophoenix@gmail.com>
2026-06-05 15:00:49 +0800

    SubjectController returns a LIST of subjects

--- demo/src/main/java/com/nix/springbootpractice/demo/controllers/SubjectController.java
+++ demo/src/main/java/com/nix/springbootpractice/demo/controllers/SubjectController.java
@@ -3,11 +3,16 @@ package com.nix.springbootpractice.demo.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class SubjectController {
     
-    @GetMapping("/subjects")
-    public String getSubjects(){
-        return "Math, Science, English";
+    //added /api to the path to follow RESTful API conventions
+    @GetMapping("/api/subjects")
+
+    // this way, the method will return a list of subjects in JSON format
+    public List<String> getSubjects(){
+        return List.of("Mathematics", "Science", "English");
     }
 }
