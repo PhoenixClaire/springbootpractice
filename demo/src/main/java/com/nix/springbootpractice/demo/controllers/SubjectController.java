434d0ff2765f77acbc39484350c494ea5e2ff24c
nix <decastrophoenix@gmail.com>
2026-06-05 15:09:41 +0800

    created a subject model class --> /api/subjects now returns a subject object with name and id

--- demo/src/main/java/com/nix/springbootpractice/demo/controllers/SubjectController.java
+++ demo/src/main/java/com/nix/springbootpractice/demo/controllers/SubjectController.java
@@ -3,6 +3,9 @@ package com.nix.springbootpractice.demo.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//import model
+import com.nix.springbootpractice.model.Subject;
+
 import java.util.List;
 
 @RestController
@@ -11,8 +14,14 @@ public class SubjectController {
     //added /api to the path to follow RESTful API conventions
     @GetMapping("/api/subjects")
 
-    // this way, the method will return a list of subjects in JSON format
-    public List<String> getSubjects(){
-        return List.of("Mathematics", "Science", "English");
+    // this way, the method will return a list of subject objects as JSON
+    public List<Subject> getSubjects(){
+        return List.of(
+            
+            //create new subject objects with id and name
+            new Subject(1l, "Math"),
+            new Subject(2l, "Science"),
+            new Subject(3L, "English")
+        );
     }
 }
---
+++ demo/src/main/java/com/nix/springbootpractice/model/Subject.java
@@ -0,0 +1,23 @@
+package com.nix.springbootpractice.model;
+
+public class Subject {
+    
+    //a subject has an id and a name
+    private Long id;
+    private String name;
+
+    //constructor
+    public Subject(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    //getters for id and name to allow spring to access these properties 
+    public Long getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
