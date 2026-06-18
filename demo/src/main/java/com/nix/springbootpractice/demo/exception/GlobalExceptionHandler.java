017b8e9b41592ce343da33688fc26db2adef8013
nix <decastrophoenix@gmail.com>
2026-06-18 20:33:20 +0800

    return cleaner validation errors

---
+++ demo/src/main/java/com/nix/springbootpractice/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.nix.springbootpractice.demo.exception;
+
+import java.util.HashMap;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//this class handles exceptions from controller
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+    //run this when validation fails --> JSON
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationErrors(MethodArgumentNotValidException ex){
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return errors;
+    }
+}
